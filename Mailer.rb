adf44c842ee4bc29a07382db412358647909e0fc
Sergey Boyko <badwolf6661@icloud.com>
2021-10-29 14:12:31 +0300

    Add spaces to comments

--- Mailer.rb
+++ Mailer.rb
@@ -27,7 +27,7 @@ class Mailer
   end
 
   def self.deliver_by_telegram(params)
-    #To send a telegram report you need to start chat with @Testrepotiobot !!!
+    # To send a telegram report you need to start chat with @Testrepotiobot !!!
 
     bot = Telegram::Bot::Client.new(TOKEN)
     bot.api.send_message(chat_id: params[:to], text: format_report(params[:report]))
@@ -59,24 +59,24 @@ end
 # report =  [
 #  { code: 'A-001', guest: 'guest@email.com', entity: 'reservation', type: 'confirmed', created_at: '2019-06-08 23:06:45', updated_at: '2019-06-08 23:06:45' },
 # { code: 'A-001', guest: 'guest@email.com', entity: 'reservation', type: 'modified', created_at: '2019-06-08 23:06:45', updated_at: '2019-06-08 23:40:02' }
-#]
+# ]
 
-#Mailer.deliver_by_mail(
+# Mailer.deliver_by_mail(
 #  to: 'testreport@bk.ru',
 #  subject: 'Report',
 #  report: {
 #    body: report,
 #    sort: 'type'
 #  }
-#)
+# )
 
-#To send a telegram report you need to start chat with @Testrepotiobot !!!
-#Mailer.deliver_by_telegram(
+# To send a telegram report you need to start chat with @Testrepotiobot !!!
+# Mailer.deliver_by_telegram(
 #  to: '733017529', # Chat_id sting or integer
 #  report: {
 #    body: report,
 #    sort: 'type' # type of sort (string) code,guest,entity,type,created_at,updated_at
 #  }
-#)
+# )
 
 
